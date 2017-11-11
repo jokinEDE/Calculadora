f4c685614d8cdac91ae362016f20c12ab5db7bd0
Loren Aparicio <loren.djkiller@gmail.com>
2017-11-12 00:24:32 +0100

    Terminada interface de calculadora en LandScape, toca empezar con los switch

--- app/src/main/java/com/example/loren/calculadora/opciones.java
+++ app/src/main/java/com/example/loren/calculadora/opciones.java
@@ -2,12 +2,27 @@ package com.example.loren.calculadora;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Switch;
+import android.widget.Toast;
 
 public class opciones extends AppCompatActivity {
+    Button mBotonSuma;
+    Switch mSwitchSuma;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.opciones);
+
+        mSwitchSuma = (Switch) findViewById(R.id.switchSuma);
+        mBotonSuma = (Button) findViewById(R.id.botonSuma);
+    }
+
+    public void onClickSwitchSuma(View v) {
+
+        Toast.makeText(this, "Que pasaaa", Toast.LENGTH_SHORT).show();
+
     }
 }
